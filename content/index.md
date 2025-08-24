92c0776dcf9d4c155c3b90f3ade4f5e34a85ba6a
guptaakashdeep <guptaakashdeep@users.noreply.github.com>
2025-08-24 11:19:12 +0530

    Quartz sync: Aug 24, 2025, 11:19 AM

---
+++ content/index.md
@@ -0,0 +1,7 @@
+---
+title: Index for TIL
+cssclasses:
+- page-brown
+---
+
+This is the index of all the TILs pages created over time. this will include tags for each note along with the dates here so it's easier to navigate
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Today I Learned",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
       typography: {
         header: "Schibsted Grotesk",
         body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        code: "DM Mono",
       },
       colors: {
         lightMode: {
