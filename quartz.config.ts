0939a9147b5603a318e03089c30b70d59e2f0894
guptaakashdeep <guptaakashdeep@users.noreply.github.com>
2025-08-24 11:29:04 +0530

    Quartz sync: Aug 24, 2025, 11:29 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Today I Learned",
+    pageTitle: "Second Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
